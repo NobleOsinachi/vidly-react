82abd7d13e1d98216a3ab7b08df8d093cce8b215
Noble Osinachi <nobleosinachi98@gmail.com>
2023-03-31 10:59:30 +0100

    Removed duplicate files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { React } from "react";
+import { ReactDOM } from "react-dom";
+import logo from './logo.svg';
 import './App.css';
-// import Movies from "./components/movies";
+import Movies from "./components/movies";
 import Counters from "./components/counters";
 
 function App() {
